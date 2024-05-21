82f30221483e0e35332e4685259790f67a02ed69
Art <d.art.shishkin@gmail.com>
2024-05-21 21:45:17 +0300

    49. Configure System Prompt and Test (#4)

--- spring-ai-rag-expert/src/main/java/net/shyshkin/study/springairagexpert/service/OpenAIService.java
+++ spring-ai-rag-expert/src/main/java/net/shyshkin/study/springairagexpert/service/OpenAIService.java
@@ -5,8 +5,10 @@ import net.shyshkin.study.springairagexpert.model.Answer;
 import net.shyshkin.study.springairagexpert.model.Question;
 import org.springframework.ai.chat.ChatClient;
 import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
@@ -24,22 +26,28 @@ public class OpenAIService implements AIService {
     private final ChatClient chatClient;
     private final VectorStore vectorStore;
 
-    @Value("classpath:/templates/rag-prompt-template-meta.st")
+    @Value("classpath:/templates/rag-prompt-template.st")
     private Resource ragPromptTemplate;
+
+    @Value("classpath:/templates/system-message.st")
+    private Resource systemMessageTemplate;
 
     @Override
     public Answer answerTheQuestion(Question question) {
 
-        SearchRequest searchRequest = SearchRequest.query(question.question()).withTopK(4);
+        PromptTemplate systemPromptTemplate = new SystemPromptTemplate(systemMessageTemplate);
+        Message systemMessage = systemPromptTemplate.createMessage();
+
+        SearchRequest searchRequest = SearchRequest.query(question.question()).withTopK(5);
         List<Document> docs = vectorStore.similaritySearch(searchRequest);
         List<String> contentList = docs.stream().map(Document::getContent).toList();
 
         PromptTemplate promptTemplate = new PromptTemplate(ragPromptTemplate);
-        Prompt prompt = promptTemplate.create(Map.of(
+        Message userMessage = promptTemplate.createMessage(Map.of(
                 "input", question.question(),
                 "documents", String.join("\n", contentList)
         ));
-        ChatResponse response = chatClient.call(prompt);
+        ChatResponse response = chatClient.call(new Prompt(List.of(systemMessage, userMessage)));
         String content = response.getResult().getOutput().getContent();
         return new Answer(content);
     }
