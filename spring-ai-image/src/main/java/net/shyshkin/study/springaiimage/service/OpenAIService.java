7128f8a85f6617a8eacf18a28cf3eea80aa6eaef
Art <d.art.shishkin@gmail.com>
2024-05-22 18:00:22 +0300

    62. Implement OpenAI Image Service (#6)

--- spring-ai-image/src/main/java/net/shyshkin/study/springaiimage/service/OpenAIService.java
+++ spring-ai-image/src/main/java/net/shyshkin/study/springaiimage/service/OpenAIService.java
@@ -2,14 +2,30 @@ package net.shyshkin.study.springaiimage.service;
 
 import lombok.RequiredArgsConstructor;
 import net.shyshkin.study.springaiimage.model.Question;
+import org.springframework.ai.image.*;
 import org.springframework.stereotype.Service;
+
+import java.util.Base64;
 
 @Service
 @RequiredArgsConstructor
 public class OpenAIService implements AIService {
 
+    private final ImageClient imageClient;
+
     @Override
     public byte[] getImage(Question question) {
-        throw new RuntimeException("NOT IMPLEMENTED YET");
+
+        ImageOptions imageOptions = ImageOptionsBuilder.builder()
+                .withHeight(1024)
+                .withWidth(1024)
+                .withResponseFormat("b64_json")
+                .build();
+
+        ImagePrompt imagePrompt = new ImagePrompt(question.question(), imageOptions);
+        ImageResponse response = imageClient.call(imagePrompt);
+
+        String b64Json = response.getResult().getOutput().getB64Json();
+        return Base64.getDecoder().decode(b64Json);
     }
 }
