1739516f394bc038f6142789072f8bd21865007b
Art <d.art.shishkin@gmail.com>
2024-05-23 14:18:06 +0300

    68. Implement Vision Service (#6)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # art-sfg-spring-ai
 Tutorial on Spring AI from SpringFrameworkGuru (Udemy)
+
+[dev_img](spring-ai-image/coinbase_api_awesome_stuck.png)
--- spring-ai-image/src/main/java/net/shyshkin/study/springaiimage/service/OpenAIService.java
+++ spring-ai-image/src/main/java/net/shyshkin/study/springaiimage/service/OpenAIService.java
@@ -3,12 +3,21 @@ package net.shyshkin.study.springaiimage.service;
 
 import lombok.RequiredArgsConstructor;
 import net.shyshkin.study.springaiimage.model.Question;
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.Media;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.image.ImageClient;
 import org.springframework.ai.image.ImageOptions;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.openai.api.OpenAiImageApi;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Base64;
@@ -18,6 +27,7 @@ import java.util.Base64;
 public class OpenAIService implements AIService {
 
     private final ImageClient imageClient;
+    private final ChatClient chatClient;
 
     @Override
     public byte[] getImage(Question question) {
@@ -26,7 +36,7 @@ public class OpenAIService implements AIService {
                 .withHeight(1024)
                 .withWidth(1024)
                 .withResponseFormat("b64_json")
-                .withModel("dall-e-3")
+                .withModel(OpenAiImageApi.ImageModel.DALL_E_3.getValue())
                 .withQuality("hd") //default - standard
                 .withStyle("natural") //default - vivid
                 .build();
@@ -40,6 +50,15 @@ public class OpenAIService implements AIService {
 
     @Override
     public String getDescription(MultipartFile file) {
-        return file.getOriginalFilename();
+        OpenAiChatOptions chatOptions = OpenAiChatOptions.builder()
+                .withModel(OpenAiApi.ChatModel.GPT_4_VISION_PREVIEW.value)
+                .build();
+
+        UserMessage userMessage = new UserMessage(
+                "Explain what do you see in this picture", //content
+                new Media(MimeTypeUtils.IMAGE_PNG, file.getResource()) //media
+        );
+        ChatResponse response = chatClient.call(new Prompt(userMessage, chatOptions));
+        return response.getResult().getOutput().toString();
     }
 }
