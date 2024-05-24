f9e3db7aad3fe96bd588951cf6a70ac3f186a9aa
Art <d.art.shishkin@gmail.com>
2024-05-24 11:59:26 +0300

    72. OpenAI Speech Client (#7)

--- spring-ai-audio/src/main/java/net/shyshkin/study/springaiaudio/service/OpenAIService.java
+++ spring-ai-audio/src/main/java/net/shyshkin/study/springaiaudio/service/OpenAIService.java
@@ -3,14 +3,25 @@ package net.shyshkin.study.springaiaudio.service;
 
 import lombok.RequiredArgsConstructor;
 import net.shyshkin.study.springaiaudio.model.Question;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.ai.openai.audio.speech.SpeechPrompt;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class OpenAIService implements AIService {
 
+    private final OpenAiAudioSpeechModel audioSpeechModel;
+
     @Override
     public byte[] getSpeech(Question question) {
-        return new byte[0];
+        OpenAiAudioSpeechOptions audioSpeechOptions = OpenAiAudioSpeechOptions.builder()
+                .withVoice(OpenAiAudioApi.SpeechRequest.Voice.FABLE)
+                .build();
+        SpeechPrompt prompt = new SpeechPrompt(question.question(), audioSpeechOptions);
+        return audioSpeechModel.call(prompt).getResult().getOutput();
     }
+
 }
